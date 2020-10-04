729f2c0e418d531f6b6dedce803354b8442ee8d3
CreativiTICs <creativiticscolombia@gmail.com>
2020-10-04 18:20:10 -0500

    Fin sección 8

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const { allowedNodeEnvironmentFlags } = require('process');
+require('dotenv').config();
+
+//DB Config
+require('./database/config').dbConnection();
+
+
+//App de Express
+const app = express();
+
+//Lectura y parseo Body Http
+app.use(express.json());
+
+
+//Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+
+//Path o Carpeta Pública
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+//Mis Rutas
+app.use('/api/login', require('./routes/auth'));
+
+
+server.listen(process.env.PORT, (err)=>{
+    if(err)throw new Error(err);
+    console.log('Servidor corriendo en puerto!!', process.env.PORT);
+});
+
+
